52ffc47502e62e9ee7ebb264255d42277b2e3d29
DAVISILVA12345 <davisilva1945@hotmail.com>
2023-10-24 20:38:41 -0300

    Ilumincao2

---
+++ src/cenario/Iluminacao.java
@@ -0,0 +1,29 @@
+package cenario;
+
+import com.jogamp.opengl.GL2;
+
+public class Iluminacao {
+
+    public boolean liga  = true;
+    public int tonalizacao = GL2.GL_SMOOTH;
+
+    public void iluminacaoAmbiente(GL2 gl){
+
+        float luzAmbiente[] = {0.2f,0.2f,0.5f,0.5f};
+        float posicaoLuz[] = {-50.0f,50.0f,100.0f,1.0f};
+
+        gl.glLightfv(GL2.GL_LIGHT0, GL2.GL_AMBIENT, luzAmbiente, 0);
+        gl.glLightfv(GL2.GL_LIGHT0, GL2.GL_POSITION, posicaoLuz, 0);
+    }
+
+    public void ligaLuz(GL2 gl){
+
+        gl.glEnable(GL2.GL_COLOR_MATERIAL);
+
+        gl.glEnable(GL2.GL_LIGHTING);
+
+        gl.glEnable(GL2.GL_LIGHT0);
+
+        gl.glShadeModel(tonalizacao);
+    }
+}
